866a56f301bc809cbec142067c79caf1786b16dc
Yago Lima Lins <yago.lima.lins@protonmail.com>
2023-03-07 03:53:45 -0300

    Add new solved challenges

---
+++ 31 - The Thing Namer 3000/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); // Gets the thing
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine(); // Gets the thing's description
+string c = "Doom"; // Stores the "Doom" string value on the c variable
+string d = "3000"; // Stores the "3000" string value on the d variable
+
+/* Finally concatenates string chunks with variable 
+ * values and prints a meaningful and powerful title */
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
