d1b6113a3c17cfe7bd62f633feb801472b5a4150
Yago Lima Lins <yago.lima.lins@protonmail.com>
2023-03-09 02:55:55 -0300

    Add new solved challenges

---
+++ 45 - The Variable Shop Returns/Program.cs
@@ -0,0 +1,67 @@
+﻿int int_number = 2147483647;
+short short_number = 32767;
+long long_number = 9223372036854775807;
+byte byte_number = 255;
+sbyte sbyte_number = 127;
+uint uint_number = 4294967295;
+ushort ushort_number = 65535;
+ulong ulong_number = 18446744073709551615;
+
+float float_number = 134.45E-2f;
+double double_number = 0.42e2;
+decimal decimal_number = 1.5E6m;
+
+char char_letter = 'a';
+string string_word = "word";
+
+bool bool_variable = false;
+
+Console.WriteLine("int: " + int_number);
+Console.WriteLine("short: " + short_number);
+Console.WriteLine("long: " + long_number);
+Console.WriteLine("byte: " + byte_number);
+Console.WriteLine("sbyte: " + sbyte_number);
+Console.WriteLine("uint: " + uint_number);
+Console.WriteLine("ushort: " + ushort_number);
+Console.WriteLine("ulong: " + ulong_number);
+Console.WriteLine("float: " + float_number);
+Console.WriteLine("double: " + double_number);
+Console.WriteLine("decimal: " + decimal_number);
+Console.WriteLine("char: " + char_letter);
+Console.WriteLine("string: " + string_word);
+Console.WriteLine("bool: " + bool_variable);
+
+Console.WriteLine();
+
+int_number = 0;
+short_number = 0;
+long_number = 0;
+byte_number = 0;
+sbyte_number = 0;
+uint_number = 0;
+ushort_number = 0;
+ulong_number = 0;
+
+float_number = 0.0f;
+double_number = 0.0;
+decimal_number = 0.0M;
+
+char_letter = 'z';
+string_word = "nothing";
+
+bool_variable = true;
+
+Console.WriteLine("int: " + int_number);
+Console.WriteLine("short: " + short_number);
+Console.WriteLine("long: " + long_number);
+Console.WriteLine("byte: " + byte_number);
+Console.WriteLine("sbyte: " + sbyte_number);
+Console.WriteLine("uint: " + uint_number);
+Console.WriteLine("ushort: " + ushort_number);
+Console.WriteLine("ulong: " + ulong_number);
+Console.WriteLine("float: " + float_number);
+Console.WriteLine("double: " + double_number);
+Console.WriteLine("decimal: " + decimal_number);
+Console.WriteLine("char: " + char_letter);
+Console.WriteLine("string: " + string_word);
+Console.WriteLine("bool: " + bool_variable);
---
+++ 45 - The Variable Shop/Program.cs
@@ -0,0 +1,32 @@
+﻿int int_number = 2147483647;
+short short_number = 32767;
+long long_number = 9223372036854775807;
+byte byte_number = 255;
+sbyte sbyte_number = 127;
+uint uint_number = 4294967295;
+ushort ushort_number = 65535;
+ulong ulong_number = 18446744073709551615;
+
+float float_number = 134.45E-2f;
+double double_number = 0.42e2;
+decimal decimal_number = 1.5E6m;
+
+char char_letter = 'a';
+string string_word = "word";
+
+bool bool_variable = false;
+
+Console.WriteLine("int: " + int_number);
+Console.WriteLine("short: " + short_number);
+Console.WriteLine("long: " + long_number);
+Console.WriteLine("byte: " + byte_number);
+Console.WriteLine("sbyte: " + sbyte_number);
+Console.WriteLine("uint: " + uint_number);
+Console.WriteLine("ushort: " + ushort_number);
+Console.WriteLine("ulong: " + ulong_number);
+Console.WriteLine("float: " + float_number);
+Console.WriteLine("double: " + double_number);
+Console.WriteLine("decimal: " + decimal_number);
+Console.WriteLine("char: " + char_letter);
+Console.WriteLine("string: " + string_word);
+Console.WriteLine("bool: " + bool_variable);
