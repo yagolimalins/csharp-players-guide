a66be7a4d3f2ecc1ff74123df01139c1ece7cb02
Yago Lima Lins <yago.lima.lins@protonmail.com>
2023-03-14 07:39:11 -0300

    Add new solved challenges

---
+++ 53 - The Triangle Farmer/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Base: ");
+double baseValue = Convert.ToDouble(Console.ReadLine());
+Console.Write("Height: ");
+double heightValue = Convert.ToDouble(Console.ReadLine());
+
+double area = (baseValue * heightValue) / 2;
+
+Console.WriteLine($"Area: {area}");
---
+++ 56 - The Four Sisters and the Duckbear/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Number of chocolate eggs: ");
+int chocolateEggsGathered = int.Parse(Console.ReadLine());
+Console.WriteLine($"Each sister gets {chocolateEggsGathered / 4} chocolate eggs");
+Console.WriteLine($"Duckbear gets {chocolateEggsGathered % 4} chocolate eggs");
